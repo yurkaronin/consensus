4efb6edbc9d8de39c4a8199ab1ab94fab6a4a2ca
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2021-08-10 20:10:42 +0300

    Блок с отзывами
    разметка и стилизация готовы

--- src/js/main.js
+++ src/js/main.js
@@ -57,3 +57,64 @@ var swiper = new Swiper(".mySwiper-3", {
     }
   }
 });
+
+var swiper = new Swiper(".mySwiper-4", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 480px
+    210: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    1350: {
+      slidesPerView: 2,
+      spaceBetween: 45,
+    }
+  }
+});
+
+// вставка видео с ютуба
+function findVideos() {
+  let e = document.querySelectorAll(".video-block")
+  for (let t = 0; t < e.length; t++) setupVideo(e[t])
+}
+function setupVideo(e) {
+  let t = e.querySelector(".video-block__link"),
+    r = e.querySelector(".video-block__media"),
+    i = e.querySelector(".video-block__button"),
+    o = parseMediaURL(r)
+  e.addEventListener("click", () => {
+    let r = createIframe(o)
+    t.remove(), i.remove(), e.appendChild(r)
+  }),
+    t.removeAttribute("href"),
+    e.classList.add("video-block--enabled")
+}
+function parseMediaURL(e) {
+  return e.src.match(
+    /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i
+  )[1]
+}
+function createIframe(e) {
+  let t = document.createElement("iframe")
+  return (
+    t.setAttribute("allowfullscreen", ""),
+    t.setAttribute("allow", "autoplay"),
+    t.setAttribute("src", generateURL(e)),
+    t.classList.add("video-block__media"),
+    t
+  )
+}
+function generateURL(e) {
+  return "https://www.youtube.com/embed/" + e + "?rel=0&showinfo=0&autoplay=1"
+}
+findVideos();
+
